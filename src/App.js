cefc1b5a94a2ee72689463f4797c97708e00b75e
Tỏn nè <oroki147@gmail.com>
2024-08-29 17:33:49 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import QRCode from "qrcode";
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function App() {
+  const [url, setUrl] = useState('');
+  const [qrcode, setQrcode] = useState("");
+
+  const GenerateQRcode = () => {
+
+    if (!url.trim()) {
+      toast('Vui lòng nhập text');
+      return;
+    }
+
+    if (url.includes(' ')) {
+      toast('không được có khoảng trắng');
+      return;
+    }
+
+    QRCode.toDataURL(url,{
+      with: 1200,
+      margin: 2,
+      color:{
+        dark:'#000000ff',
+        light:'#ffffffff'
+      }
+    }, (err, url) => {
+      if (err) return toast(err.message);
+    //  url === undefined && console.log('djtmemay');
+      
+      console.log(url);
+      setQrcode(url);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>QR Code Generator</h1>
+      <input
+        type="text"
+        value={url}
+        onChange={(evt) => setUrl(evt.target.value)}
+        placeholder="Input text you want to gen"
+      />
+      <button onClick={GenerateQRcode}>Generate</button>
+      {qrcode ? <>
+        <img src={qrcode} />
+        <a href={qrcode} download='qrcode.png' >Down load QRCode</a>
+      </> : <>
+      </>}
+      <ToastContainer />
     </div>
   );
 }
